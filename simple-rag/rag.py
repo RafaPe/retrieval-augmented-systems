696b0064c844e45d7a8c85fe5693bc0e9650a487
RafaPe <tinoco21.30@gmail.com>
2025-12-25 16:50:38 -0600

    feat: :zap: Updated RAG to calculate TRUE metric of each answer given

--- simple-rag/rag.py
+++ simple-rag/rag.py
@@ -3,37 +3,52 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms import Ollama
+from evaluation.TRUE import TRUEEvaluator
 import os
+
+VECTOR_DB_PATH = "simple-rag/data/vectordb/faiss"
 
 llm = Ollama(
     model="llama3",
     base_url="http://localhost:11434"
 )
 
-pdf_folder = "data/pdfs"
-documents = []
-
-for file in os.listdir(pdf_folder):
-    if file.endswith(".pdf"):
-        loader = PyPDFLoader(os.path.join(pdf_folder, file))
-        documents.extend(loader.load())
-
-print(f"Loaded {len(documents)} pages")
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,
-    chunk_overlap=100
-)
-
-chunks = text_splitter.split_documents(documents)
-print(f"Created {len(chunks)} text chunks")
-
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-vectorstore = FAISS.from_documents(chunks, embeddings)
+if os.path.exists(VECTOR_DB_PATH):
+    print("Loading existing FAISS vector store...")
+    vectorstore = FAISS.load_local(
+        VECTOR_DB_PATH,
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
+else:
+    print("Creating new FAISS vector store...")
+
+    pdf_folder = "simple-rag/data/pdfs"
+    documents = []
+
+    for file in os.listdir(pdf_folder):
+        if file.endswith(".pdf"):
+            loader = PyPDFLoader(os.path.join(pdf_folder, file))
+            documents.extend(loader.load())
+
+    print(f"Loaded {len(documents)} pages")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    print(f"Created {len(chunks)} text chunks")
+
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+    vectorstore.save_local(VECTOR_DB_PATH)
+    print("Vector store saved.")
 
 llm = Ollama(model="llama3")
 
@@ -60,3 +75,9 @@ while True:
 
     response = llm.invoke(prompt)
     print("\nAnswer:\n", response)
+
+    evaluator = TRUEEvaluator()
+    score = evaluator.score(response, context)
+    print(f"\nTRUE Score: {score:.2f}")
+
+#using ONLY the context below
